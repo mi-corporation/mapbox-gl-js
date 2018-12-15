b28eddb3f00c569abb2f6bd4af50450c1f1799be
Chris Loer <chris.loer@gmail.com>
2018-12-14 17:18:50 -0800

    Add docs. Add min/max fraction digits Auto-determine "style" based on presence of "currency"

--- src/style-spec/expression/definitions/number_format.js
+++ src/style-spec/expression/definitions/number_format.js
@@ -30,21 +30,29 @@ declare class Intl$NumberFormat {
 type NumberFormatOptions = {
     style?: 'decimal' | 'currency' | 'percent';
     currency?: null | string;
+    minimumFractionDigits?: null | string;
+    maximumFractionDigits?: null | string;
 };
 
 export default class NumberFormat implements Expression {
     type: Type;
     number: Expression;
-    style: Expression;
     locale: Expression | null;   // BCP 47 language tag
     currency: Expression | null; // ISO 4217 currency code, required if style=currency
+    minFractionDigits: Expression | null; // Default 0
+    maxFractionDigits: Expression | null; // Default 3
 
-    constructor(number: Expression, style: Expression, locale: Expression | null, currency: Expression | null) {
+    constructor(number: Expression,
+                locale: Expression | null,
+                currency: Expression | null,
+                minFractionDigits: Expression | null,
+                maxFractionDigits: Expression | null) {
         this.type = StringType;
         this.number = number;
-        this.style = style;
         this.locale = locale;
         this.currency = currency;
+        this.minFractionDigits = minFractionDigits;
+        this.maxFractionDigits = maxFractionDigits;
     }
 
     static parse(args: Array<mixed>, context: ParsingContext): ?Expression {
@@ -58,10 +66,6 @@ export default class NumberFormat implements Expression {
         if (typeof options !== "object" || Array.isArray(options))
             return context.error(`NumberFormat options argument must be an object.`);
 
-        const style = context.parse(
-            options['style'] === undefined ? "decimal" : options['style'], 1, StringType);
-        if (!style) return null;
-
         let locale = null;
         if (options['locale']) {
             locale = context.parse(options['locale'], 1, StringType);
@@ -74,25 +78,44 @@ export default class NumberFormat implements Expression {
             if (!currency) return null;
         }
 
-        return new NumberFormat(number, style, locale, currency);
+        let minFractionDigits = null;
+        if (options['min-fraction-digits']) {
+            minFractionDigits = context.parse(options['min-fraction-digits'], 1, NumberType);
+            if (!minFractionDigits) return null;
+        }
+
+        let maxFractionDigits = null;
+        if (options['max-fraction-digits']) {
+            maxFractionDigits = context.parse(options['max-fraction-digits'], 1, NumberType);
+            if (!maxFractionDigits) return null;
+        }
+
+        return new NumberFormat(number, locale, currency, minFractionDigits, maxFractionDigits);
     }
 
     evaluate(ctx: EvaluationContext) {
         return new Intl.NumberFormat(this.locale ? this.locale.evaluate(ctx) : [],
             {
-                style: this.style.evaluate(ctx),
-                currency: this.currency ? this.currency.evaluate(ctx) : null
+                style: this.currency ? "currency" : "decimal",
+                currency: this.currency ? this.currency.evaluate(ctx) : null,
+                minimumFractionDigits: this.minFractionDigits ? this.minFractionDigits.evaluate(ctx) : undefined,
+                maximumFractionDigits: this.maxFractionDigits ? this.maxFractionDigits.evaluate(ctx) : undefined,
             }).format(this.number.evaluate(ctx));
     }
 
     eachChild(fn: (Expression) => void) {
         fn(this.number);
-        fn(this.style);
         if (this.locale) {
             fn(this.locale);
         }
         if (this.currency) {
             fn(this.currency);
+        }
+        if (this.minFractionDigits) {
+            fn(this.minFractionDigits);
+        }
+        if (this.maxFractionDigits) {
+            fn(this.maxFractionDigits);
         }
     }
 
@@ -102,12 +125,17 @@ export default class NumberFormat implements Expression {
 
     serialize() {
         const options = {};
-        options['style'] = this.style.serialize();
         if (this.locale) {
             options['locale'] = this.locale.serialize();
         }
         if (this.currency) {
             options['currency'] = this.currency.serialize();
+        }
+        if (this.minFractionDigits) {
+            options['min-fraction-digits'] = this.minFractionDigits.serialize();
+        }
+        if (this.maxFractionDigits) {
+            options['max-fraction-digits'] = this.maxFractionDigits.serialize();
         }
         return ["number-format", this.number.serialize(), options];
     }

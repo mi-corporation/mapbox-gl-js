18964b00ea5d87690ca2d9befa2d9c92cc500ec6
Adam Haile <adam.haile@gmail.com>
2018-12-20 08:10:37 -0500

    initial inclusion of mbtiles source type

--- src/source/worker.js
+++ src/source/worker.js
@@ -42,6 +42,7 @@ export default class Worker {
 
         this.workerSourceTypes = {
             vector: VectorTileWorkerSource,
+            mbtiles: VectorTileWorkerSource,
             geojson: GeoJSONWorkerSource
         };
 
